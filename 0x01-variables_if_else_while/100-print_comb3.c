58b722dec57d76e90ee30077bfdc62b6f8b3be0f
Micky309 <wesleymichael@yahoo.com>
2023-07-16 09:42:39 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,19 @@
  */
 int main(void)
 {
-	int n, m;
+	int i, k = '0', j;
 
-	for (n = 48; n <= 56; n++)
-		for (m = 49; m <= 57; m++)
-			if (m > n)
-				putchar(n);
-	putchar(m);
-	if (n != 56 || m != 57)
+	for (i = '0'; i <= '9'; i++)
+		for (j = k; j <= '9'; j++)
+			if (i != j)
+				putchar(i);
+	putchar(j);
+	if (i == j)
+		if (i == '8' && j == '9')
+		else
 		putchar(',');
 			putchar(' ');
+			k++;
 	putchar('\n');
 	return (0);
 }
