23866de2f3c9ba8fad90503f758167011e40d1ec
Micky309 <wesleymichael@yahoo.com>
2023-07-26 17:07:27 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,9 +6,9 @@
  */
 char *leet(char *n)
 {
-        int i, j;
-        char s1[] = "aAeEo0tTlL";
-        char s2[] = "4433007711";
+	int i, j;
+	char s1[] = "aAeEo0tTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
